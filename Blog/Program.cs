b2cda12a139791e23158bbbed2003484197f3f82
BrunoMartins237 <bcamilomenezes@gmail.com>
2022-01-25 19:51:20 -0300

    Iniciando Projeto com Entity Framework

---
+++ Blog/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new BlogDataContext())
+            {
+                //CREATE
+                // var tag = new Tag{Name="SUCRILHOS", Slug="sucrilhos"};
+                // context.Tags.Add(tag);
+                // context.SaveChanges();
+
+                //UPDATE
+                // var tag = context.Tags.FirstOrDefault(x=>x.Id == 3);
+                // tag.Name = ".NET";
+                // tag.Slug = "dotnet";
+                // context.Tags.Update(tag);
+                // context.SaveChanges();
+
+                //DELETE
+                var tag = context.Tags.FirstOrDefault(x=>x.Id == 3);
+                context.Remove(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
